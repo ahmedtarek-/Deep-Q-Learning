51e356f573fbb901c85139d98fa0556e39749a0d
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2023-07-28 12:17:08 +0200

    Yet another result fix

--- training/result.py
+++ training/result.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Result():
   RESULTS_ROOT_DIR = "results"
-  MODELS_DIR = RESULTS_ROOT_DIR + "/" + "models"
+  MODELS_DIR = self.RESULTS_ROOT_DIR + "/" + "models"
 
   def __init__(self, model, rewards, disc_rewards, env_name):
     self.model = model
@@ -21,7 +21,7 @@ class Result():
 
     if plot:
       plt.plot(self.rewards)
-      plt.savefig(RESULTS_ROOT_DIR + "/" + "plots" + "/" + self.file_prefix() + ".png")
+      plt.savefig(self.RESULTS_ROOT_DIR + "/" + "plots" + "/" + self.file_prefix() + ".png")
 
   def file_prefix(self):
     now = datetime.now()
